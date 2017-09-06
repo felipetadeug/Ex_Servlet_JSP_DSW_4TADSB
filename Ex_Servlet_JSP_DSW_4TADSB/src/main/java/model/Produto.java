d88163e9918632657b78687bcc13f03c98415a35
DocSpider01 <DocSpider01@192.168.1.36>
2017-09-06 16:33:38 -0300

    Criação da Model Produto e Servlet listarProduto (estrutura)

---
+++ Ex_Servlet_JSP_DSW_4TADSB/src/main/java/model/Produto.java
@@ -0,0 +1,76 @@
+package model;
+
+public class Produto implements java.io.Serializable{
+   
+    private int id;
+    private String nome;
+    private String descricao;
+    private double preco;
+
+    public Produto(int id, String nome, String descricao, double preco) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.preco = preco;
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the descricao
+     */
+    public String getDescricao() {
+        return descricao;
+    }
+
+    /**
+     * @param descricao the descricao to set
+     */
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    /**
+     * @return the preco
+     */
+    public double getPreco() {
+        return preco;
+    }
+
+    /**
+     * @param preco the preco to set
+     */
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+    
+    
+
+   
+}
